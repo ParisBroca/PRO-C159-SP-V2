e43b56990c7a0280c96e46eb52097e2fceff759f
BYJUS-smah <95708613+BYJUS-smah@users.noreply.github.com>
2022-06-13 17:19:57 -0500

    Update infoBanner.js

--- js/infoBanner.js
+++ js/infoBanner.js
@@ -16,14 +16,14 @@ AFRAME.registerComponent("info-banner", {
       },
       spiderman: {
         banner_url: "./assets/posters/spiderman-banner.png",
-        title: "Hombre-Araña",
+        title: "Spider-Man",
         released_year: "1962",
         description:
-          "El Hombre Araña es un superhéroe ficticio creado por el escritor y editor Stan Lee y el escritor y artista Steve Ditko. Apareció por primera vez en el cómic de antología Amazing Fantasy (agosto de 1962) en la Edad de Plata de las historietas.",
+          "Spider-Man es un superheroe ficticio creado por el escritor y editor Stan Lee y el escritor y artista Steve Ditko. Aparece por primera vez en la historieta de antologia Amazing Fantasy (agosto de 1962) en la Edad de Plata de las historietas.",
       },
       "captain-aero": {
         banner_url: "./assets/posters/captain-aero-banner.jpg",
-        title: "Capitn Aero",
+        title: "Capitan Aero",
         released_year: "1942",
         description:
           "Capitán Aero es una historieta de la Edad de Oro del Cómic, publicado originalmente por Helnit Publishing y adquirido por Holyoke Publishing en 1942. El número se publicó en diciembre de 1941, y se mantuvo hasta el agosto de 1946.",
